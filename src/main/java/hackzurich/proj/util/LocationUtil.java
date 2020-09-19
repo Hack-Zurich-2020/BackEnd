41113e63988a98fdc8ca533a7e2fb764dc68ddc8
y.zouroufchi <y.zouroufchi@mohaymen.co>
2020-09-19 14:22:13 +0430

    Add LocationUtil

---
+++ src/main/java/hackzurich/proj/util/LocationUtil.java
@@ -0,0 +1,15 @@
+package hackzurich.proj.util;
+
+public class LocationUtil {
+    public static double calcDistance(double lat1, double lon1, double lat2, double lon2) {
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double distance = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return distance;
+    }
+}
