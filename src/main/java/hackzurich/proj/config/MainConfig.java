e8801027efaf47534e13784356fc66b32fd3832f
y.zouroufchi <y.zouroufchi@mohaymen.co>
2020-09-19 21:24:59 +0430

    Fix MainConfig

--- src/main/java/hackzurich/proj/config/MainConfig.java
+++ src/main/java/hackzurich/proj/config/MainConfig.java
@@ -1,11 +1,13 @@
 package hackzurich.proj.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Data
 public class MainConfig {
+    @Value("${max-distance-km}")
     private int maxDistanceKm;
 }
