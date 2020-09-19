ed03f54ea1aa24ff45263c07ac1a6fbe3145d4da
y.zouroufchi <y.zouroufchi@mohaymen.co>
2020-09-19 14:23:06 +0430

    SpringBoot application created with its properties

---
+++ src/test/java/hackzurich/proj/controller/ControllerTest.java
@@ -0,0 +1,18 @@
+package hackzurich.proj.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllerTest {
+    private RestTemplate restTemplate;
+
+    @Test
+    public void foodInquiryTest(){
+
+    }
+}
